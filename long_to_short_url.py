0fdce157c3985b373d2bd21bdd5317ae29313171
Dmitry Agafonov <malkiz@yandex.ru>
2021-05-06 07:02:54 +0300

    Initial commit

--- long_to_short_url.py
+++ long_to_short_url.py
@@ -1,5 +1,46 @@
+import sqlite3
 import settings
 
 
-def take_link_from_db():
-    pass
+def take_combination_from_db() -> tuple[int, str]:
+    conn = sqlite3.connect('databases/' + settings.DB_COMBINATIONS_NAME)
+    cursor = conn.cursor()
+    cursor.execute("""SELECT id, short_combination FROM combinations
+    WHERE id = (SELECT MAX(id) FROM combinations)""")
+    combination_id, short_combination = cursor.fetchall()[0]
+    return combination_id, short_combination
+
+
+def delete_last_combination_from_db(last_combination_id: int) -> None:
+    conn = sqlite3.connect('databases/' + settings.DB_COMBINATIONS_NAME)
+    cursor = conn.cursor()
+    cursor.execute("""DELETE FROM combinations
+    WHERE id = (?)""", (last_combination_id, ))
+    conn.commit()
+
+
+def get_combination():
+    combination_id, short_combination = take_combination_from_db()
+    delete_last_combination_from_db(combination_id)
+    return short_combination
+
+
+def add_combination_to_urls_db(long_url: str) -> str:
+    conn = sqlite3.connect('databases/' + settings.DB_URLS_NAME)
+    cursor = conn.cursor()
+    url = make_correct_url(long_url)
+    combination = get_combination()
+    cursor.execute("""INSERT INTO urls (long_url, short_combination)
+    VALUES (?, ?)""", (url, combination, ))
+    conn.commit()
+    return f'{settings.DOMAIN_NAME}/{combination}'
+
+
+def make_correct_url(url: str) -> str:
+    if url.startswith('http://') or url.startswith('https://'):
+        return url
+    return 'http://' + url
+
+
+def make_a_short_url(long_url: str) -> str:
+    return add_combination_to_urls_db(long_url)
