ed690b15b021ed763cc8e12882d7f56406b8427d
Dmitry Agafonov <malkiz@yandex.ru>
2021-05-06 07:04:29 +0300

    Add redirect into short_link, some code improvements

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import long_to_short_url
+import short_to_long_url
 
 """
 Необходимо создать базу, в которую будем помещать ссылки от пользователей (проверять на наличие точки через regexp?)
@@ -12,18 +14,22 @@ app = Flask(__name__)
 @app.route('/', methods=['post', 'get'])
 def login():
     message = ''
+    short_url = ''
     if request.method == 'POST':
-        long_link = request.form.get('long_link')  # запрос к данным формы
-        message = f'Вижу твою ссылку, это {long_link}'
+        long_url = request.form.get('long_url')  # запрос к данным формы
+        short_url = long_to_short_url.make_a_short_url(long_url)
+        return short_url
+    return render_template('login.html', message=message, short_url=short_url)
 
-    return render_template('login.html', message=message)
 
-
-@app.route('/<short_link>/')
-def short_link(link=None):
-    print(link)
+@app.route('/<short_url>/')
+def short_url(short_url):
+    long_url = short_to_long_url.return_original_url_from_short(short_url)
+    if long_url:
+        return redirect(f'{long_url}')
+    return 'Такой ссылки нет :('
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0')
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
+    # app.run(debug=True)
