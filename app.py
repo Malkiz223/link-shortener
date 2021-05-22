dbbc2ac4b31804ba576d600eb8dea2206129f630
Dmitry Agafonov <malkiz@yandex.ru>
2021-05-22 13:56:20 +0300

    Add AJAX request handling

--- app.py
+++ app.py
@@ -1,37 +1,28 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 import long_to_short_url
 import short_to_long_url
-
-"""
-Необходимо создать базу, в которую будем помещать ссылки от пользователей (проверять на наличие точки через regexp?)
-При переходе по /<short_link> делать селект из БД и возвращать значение full_link в качестве перенаправления
-Сделать базу сгенерированных линков
-"""
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['post', 'get'])
-def login():
-    message = ''
-    short_url = ''
+def index():
     if request.method == 'POST':
         long_url = request.form.get('long_url')  # запрос к данным формы
         if not long_url:
-            return 'Пожалуйста, введите URL'
-        short_url = long_to_short_url.make_a_short_url(long_url)
-        return short_url
-    return render_template('login.html', message=message, short_url=short_url)
+            return jsonify({'error': 'Введите, пожалуйста, URL'})
+        short_url_for_user = long_to_short_url.make_a_short_url(long_url)
+        return jsonify({'message': short_url_for_user})
+    return render_template('login.html')
 
 
-@app.route('/<short_url>/')
-def short_url(short_url):
-    long_url = short_to_long_url.return_original_url_from_short(short_url)
+@app.route('/<short_url_from_user>')
+def short_url(short_url_from_user):
+    long_url = short_to_long_url.return_original_url_from_short(short_url_from_user)
     if long_url:
-        return redirect(f'{long_url}')
-    return 'Такой ссылки нет :('
+        return redirect(long_url)
+    return 'Такой ссылки нет :(', 404
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-    # app.run(debug=True)
