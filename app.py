3821e686f3395c76c7d3a0ef1405894a8c3ca10f
Dmitry Agafonov <malkiz@yandex.ru>
2021-05-28 22:32:24 +0300

    Edit docstring

--- app.py
+++ app.py
@@ -27,11 +27,11 @@ def index_post():
         return jsonify({'error': 'Ссылка не может быть пустой'})
     long_url = long_url.strip()
 
-    has_dot_regexp = r'\w+[.]\w\w+'  # имеет символ до точки и минимум два символа после точки
+    has_dot_regexp = r'\w+[.]\w\w+'  # имеет символ, точку и минимум два символа после точки
     has_any_spaces_regexp = r'\s+'  # имеет пробельные символы в ссылке
-    has_a_dot_and_two_characters_after = re.search(has_dot_regexp, long_url)
+    has_dot_and_two_characters_after = re.search(has_dot_regexp, long_url)
     has_any_spaces = re.search(has_any_spaces_regexp, long_url)
-    if not has_a_dot_and_two_characters_after or has_any_spaces:
+    if not has_dot_and_two_characters_after or has_any_spaces:
         return jsonify({'error': 'Некорректная ссылка'})
 
     # имеет в ссылке собственное доменное имя
@@ -48,7 +48,7 @@ def index_post():
 @app.route('/<short_url>')
 def redirect_to_long_url(short_url):
     """
-    Функция идёт в базу и ищет там комбинацию символов, равной ссылке, которая пришла от пользователя.
+    Функция идёт в базу и ищет там комбинацию, которая пришла от пользователя.
     Если комбинация находится - возвращает ссылку, которая привязана к ней. В противном случае возвращает 404.
     """
     long_url = url_converter.convert_to_long_url(short_url)
