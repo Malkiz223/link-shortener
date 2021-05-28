da826a391df8039820b39d1c0dca2c7b7d2a1ada
Dmitry Agafonov <malkiz@yandex.ru>
2021-05-28 22:00:01 +0300

    Refactor code

--- app.py
+++ app.py
@@ -27,33 +27,31 @@ def index_post():
         return jsonify({'error': 'Ссылка не может быть пустой'})
     long_url = long_url.strip()
 
-    # имеет символ до точки и минимум два символа после точки
-    has_dot_regexp = r'\w+[.]\w\w+'
-    # имеет пробельные символы в ссылке
-    has_any_spaces_regexp = r'\s+'
+    has_dot_regexp = r'\w+[.]\w\w+'  # имеет символ до точки и минимум два символа после точки
+    has_any_spaces_regexp = r'\s+'  # имеет пробельные символы в ссылке
     has_a_dot_and_two_characters_after = re.search(has_dot_regexp, long_url)
     has_any_spaces = re.search(has_any_spaces_regexp, long_url)
     if not has_a_dot_and_two_characters_after or has_any_spaces:
         return jsonify({'error': 'Некорректная ссылка'})
 
-    # имеет ссылку для сокращения на самого себя
+    # имеет в ссылке собственное доменное имя
     has_own_url = re.match(rf'{DOMAIN_NAME}', long_url.lower()) or re.match(
         rf'{HYPERTEXT_PROTOCOL + DOMAIN_NAME}', long_url.lower())
     if has_own_url:
         return jsonify({'error': 'Нельзя сокращать ссылку на сам сайт'})
 
     # если все проверки выше пройдены - ссылка отправляется в базу, возвращая сокращённую ссылку
-    short_url_for_user = url_converter.make_a_short_url(long_url)
-    return jsonify({'message': HYPERTEXT_PROTOCOL + short_url_for_user})
+    short_url = url_converter.convert_to_short_url(long_url)
+    return jsonify({'message': HYPERTEXT_PROTOCOL + short_url})
 
 
-@app.route('/<short_url_from_user>')
-def short_url(short_url_from_user):
+@app.route('/<short_url>')
+def redirect_to_long_url(short_url):
     """
     Функция идёт в базу и ищет там комбинацию символов, равной ссылке, которая пришла от пользователя.
     Если комбинация находится - возвращает ссылку, которая привязана к ней. В противном случае возвращает 404.
     """
-    long_url = url_converter.original_url_from_short(short_url_from_user)
+    long_url = url_converter.convert_to_long_url(short_url)
     if long_url:
         return redirect(long_url)
     return render_template('404.html'), 404
